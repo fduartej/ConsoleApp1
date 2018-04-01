188f87cf6fa692f4e8392f46a6289ff1f016155b
Fred <fduartej@gmail.com>
2018-03-31 19:27:03 -0500

    Version 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+
 		}
 	}
 }
